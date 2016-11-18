c6ee13783ee0ffe80753382df6ec5a698dbb7918
Martin-Lv <lvmenglineric@gmail.com>
2016-11-18 18:01:39 +0800

    Initial Commit

---
+++ MLTimerExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MLTimerExample
+//
+//  Created by 吕孟霖 on 2016/11/18.
+//  Copyright © 2016年 Martin-Lv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
